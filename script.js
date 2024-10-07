de7eaa489343b6aa7839c6b350934a17155746dd
kingkongclover <anton.reijonen@gmail.com>
2024-10-07 21:27:42 +0300

    Testiyritys

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-
-
-const fetchData = async () => {
+async function fetchData() {
     // Haetaan tiedot data muuttujaan tvURL välimuuttuja, johon voidaan laittaa userinput
     const URL = `https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326`
     const response = await fetch(URL);
     const data = await response.json();
-
-    console.log(data);
-
     initMap(data)
-}
 
-const initMap = (data) => {
     let map = L.map('map', {
         minZoom: -3
     })
@@ -28,12 +21,41 @@ const initMap = (data) => {
 
     map.fitBounds(geoJson.getBounds())
 
-}
-
-const getFeature = (feature, layer) => {
     if (!feature.properties.nimi) return;
     const nimi = feature.properties.nimi;
     layer.bindTooltip(nimi);
+
 }
 
-fetchData();
+// const fetchData = async () => {
+    // Haetaan tiedot data muuttujaan tvURL välimuuttuja, johon voidaan laittaa userinput
+//    const URL = `https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326`
+//    const response = await fetch(URL);
+//    const data = await response.json();
+//    initMap(data)
+//}
+
+//const initMap = (data) => {
+//    let map = L.map('map', {
+//        minZoom: -3
+//    })
+//
+//    let geoJson = L.geoJSON(data, {
+//        weight: 2,
+//        onEachFeature: getFeature
+ //   }).addTo(map);
+//
+  //  let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+   //     maxZoom: 19,
+  //      attribution: "© OpenStreetMap"
+ //   }).addTo(map);
+//
+ //   map.fitBounds(geoJson.getBounds())
+//
+//}
+
+//const getFeature = (feature, layer) => {
+//    if (!feature.properties.nimi) return;
+//   const nimi = feature.properties.nimi;
+//   layer.bindTooltip(nimi);
+//}
