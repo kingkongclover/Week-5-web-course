e85d6e3210a4c10f5942b474190994db835efabd
kingkongclover <anton.reijonen@gmail.com>
2024-10-07 21:36:57 +0300

    Toimisko yippee

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-async function fetchData() {
+const nappaaData = async () => {
     // Haetaan tiedot data muuttujaan tvURL välimuuttuja, johon voidaan laittaa userinput
     const URL = `https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326`
     const response = await fetch(URL);
     const data = await response.json();
     initMap(data)
+}
 
+const initMap = (data) => {
     let map = L.map('map', {
         minZoom: -3
     })
 
     let geoJson = L.geoJSON(data, {
         weight: 2,
-        onEachFeature: getFeature
+        onEachFeature: getFeature,
     }).addTo(map);
 
     let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -21,41 +23,12 @@ async function fetchData() {
 
     map.fitBounds(geoJson.getBounds())
 
+}
+
+const getFeature = (feature, layer) => {
     if (!feature.properties.nimi) return;
     const nimi = feature.properties.nimi;
     layer.bindTooltip(nimi);
-
 }
 
-// const fetchData = async () => {
-    // Haetaan tiedot data muuttujaan tvURL välimuuttuja, johon voidaan laittaa userinput
-//    const URL = `https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326`
-//    const response = await fetch(URL);
-//    const data = await response.json();
-//    initMap(data)
-//}
-
-//const initMap = (data) => {
-//    let map = L.map('map', {
-//        minZoom: -3
-//    })
-//
-//    let geoJson = L.geoJSON(data, {
-//        weight: 2,
-//        onEachFeature: getFeature
- //   }).addTo(map);
-//
-  //  let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-   //     maxZoom: 19,
-  //      attribution: "© OpenStreetMap"
- //   }).addTo(map);
-//
- //   map.fitBounds(geoJson.getBounds())
-//
-//}
-
-//const getFeature = (feature, layer) => {
-//    if (!feature.properties.nimi) return;
-//   const nimi = feature.properties.nimi;
-//   layer.bindTooltip(nimi);
-//}
+nappaaData();
